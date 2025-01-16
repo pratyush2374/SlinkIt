f3f20513cdae1176d8fd2449cbd0c140fc653a60
pratyush2374 <pratyushsharma2374@gmail.com>
2025-01-16 23:10:13 +0530

    More bug fixes for server prod

--- server/bundle.js
+++ server/bundle.js
@@ -1,26 +1,3 @@
-// import esbuild from "esbuild";
-
-// esbuild
-//     .build({
-//         entryPoints: ["src/index.ts"], // Entry point for your server
-//         bundle: true, // Bundle all dependencies
-//         platform: "node", // Target Node.js environment
-//         target: "es2020", // Match your tsconfig target
-//         outfile: "dist/server.js", // Output bundled file
-//         external: [
-//             "@prisma/client", // Prisma client
-//             "mock-aws-s3", // Mark unresolved dependencies as external
-//             "aws-sdk",
-//             "nock",
-//             "*.html", // Ignore .html files
-//         ],
-//         loader: {
-//             // Handle specific file types
-//             ".html": "text",
-//         },
-//     })
-//     .catch(() => process.exit(1));
-
 import esbuild from "esbuild";
 
 esbuild
@@ -29,7 +6,7 @@ esbuild
         bundle: true,
         platform: "node",
         target: "es2020",
-        format: "cjs", // Output CommonJS format
+        format: "cjs", 
         outfile: "dist/server.js",
         external: [
             "@prisma/client",
